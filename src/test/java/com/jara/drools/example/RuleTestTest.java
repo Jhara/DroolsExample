0122220cb998edc2082bf8d2a5060e132018e3d1
Diego Alejandro Jaramillo Rendon <diego.jaramillo@ceiba.com.co>
2015-08-21 22:36:48 -0500

    Primer test

---
+++ src/test/java/com/jara/drools/example/RuleTestTest.java
@@ -0,0 +1,17 @@
+package com.jara.drools.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jara on 21/08/15.
+ */
+public class RuleTestTest {
+
+    @Test
+    public void test(){
+        fail();
+    }
+
+}
